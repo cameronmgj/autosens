e698b311207ae6515d9fa0d9ba01f3d84a04cf9c
cameronMG-J <cameron.mcgrath.johnston@gmail.com>
2026-06-30 20:52:52 +0100

    systems design
    -created basic code stubs for key functions
    
    -created basic data file as csv

---
+++ autosens.py
@@ -0,0 +1,23 @@
+# check if there are new games in the list from steam
+def checkNewGames():
+    pass
+
+# calculate the sensitivity for a new game based on the stored sensitivity
+# constant and multiplier for the new game
+def calculateGameMouseSpeed():
+    pass
+
+# write to game cvars/data file to edit mouse sensitivity setting value
+def editGameSetting():
+    pass
+
+# if the game file is not where it should be stored, request user input
+def gameFileNotFound():
+    pass
+
+# if the steam library file is not found, request user input
+def steamNotFound():
+    pass
+
+def main():
+    pass
---
+++ utils.py
@@ -0,0 +1,23 @@
+# reading game file contents, reading stored data in autosensdata.csv
+def fileReader():
+    pass
+
+# write to files
+def fileWriter():
+    pass
+
+# write to autosensdata.csv to add games to the list
+def updateStoredData():
+    pass
+
+# find steam's file path
+def getSteamInstall():
+    pass
+
+# check steam file paths from steam
+def getFilePaths():
+    pass
+
+# obtain a new games multiplier from the internet
+def getNewGameMultiplierValue():
+    pass
